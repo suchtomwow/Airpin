43f0f5e070e5223f5d5bf28b08ab39a6fe18fd2d
Thomas Carey <jthomascarey@gmail.com>
2018-05-17 20:00:48 -0600

    Add token entry and get token button

--- Airpin/Settings/SettingsViewController.swift
+++ Airpin/Settings/SettingsViewController.swift
@@ -6,6 +6,39 @@
 //  Copyright © 2018 Thomas Carey. All rights reserved.
 //
 
-class SettingsViewController: BaseViewController {
-    
+import Eureka
+import SafariServices
+
+class SettingsViewController: FormViewController {
+    private let entertokenRow = TextRow()
+    private let getTokenRow = ButtonRow()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        entertokenRow.title = "Pinboard token"
+        entertokenRow.placeholder = "username:token"
+        getTokenRow.title = "Get token"
+
+        getTokenRow.onCellSelection { [unowned self] _, _ in
+            guard let url = URL(string: "https://m.pinboard.in/settings/password") else {
+                return
+            }
+
+            let svc = SFSafariViewController(url: url)
+            self.present(svc, animated: true, completion: nil)
+        }
+
+        configureForm()
+    }
+
+    private func configureForm() {
+        var enterTokenSection = Section(footer: "Enter your Pinboard token to access your personal bookmarks.")
+        enterTokenSection += [entertokenRow]
+
+        var getTokenSection = Section(footer: "After signing in, navigate to Settings > Password to find your Pinboard token.")
+        getTokenSection += [getTokenRow]
+
+        form +++ enterTokenSection +++ getTokenSection
+    }
 }
